591812778b0dded4b9680006514546e22b0dcdac
Aman Mathur <amanmathur99@gmail.com>
2019-04-01 00:02:39 -0400

    commented code and updated destructors

---
+++ Lab4/cmake-build-debug/CMakeFiles/Lab4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab4.dir/lab4_main.cpp.o"
+  "Lab4.pdb"
+  "Lab4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
